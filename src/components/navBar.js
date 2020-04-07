fdec921545449493ed42c513397683191c699d6e
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-04-07 22:27:58 +0530

    dynamic Navbar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-function NavBar(props) {
+import './navBar.css';
+
+function NavBar({title}) {
     return (
         <nav className="header navbar navbar-dark bg-dark">
         <div className="container">
             <div className="row m-auto">
                 <i className="fa fa-film fa-2x text-white my-auto"></i>
-                <div className="h3 ml-3 my-auto text-light" href="/"> React </div>
+                <div className="h3 ml-3 my-auto text-light" href="/"> {title} </div>
             </div>
         </div>
     </nav>
