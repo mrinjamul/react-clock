e2ec9636c909009e5ddce550330d45cacfaa93a4
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-04-07 22:39:39 +0530

    Updates

---
+++ precache-manifest.ccca73895e4e0dc35dd8b4f152f83fdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e9a38696cae487673cae172dece4ea3",
+    "url": "/react-clock/index.html"
+  },
+  {
+    "revision": "eca1ec53e196c048f1dc",
+    "url": "/react-clock/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "2016bcdd80f9bd6a84c5",
+    "url": "/react-clock/static/css/main.ccb153f8.chunk.css"
+  },
+  {
+    "revision": "eca1ec53e196c048f1dc",
+    "url": "/react-clock/static/js/2.f3b05995.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-clock/static/js/2.f3b05995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2016bcdd80f9bd6a84c5",
+    "url": "/react-clock/static/js/main.c7080bbc.chunk.js"
+  },
+  {
+    "revision": "88998f01e026fb0f55a7",
+    "url": "/react-clock/static/js/runtime-main.487398c0.js"
+  }
+]);
